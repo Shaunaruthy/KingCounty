8c9c9cba9285a808d0706769db89b343f2905943
makenzieforster <114077223+makenzieforster@users.noreply.github.com>
2022-11-02 14:27:08 +0000

    Add files via upload

---
+++ Exploratory Code/KingCountyesales.R
@@ -0,0 +1,9 @@
+# https://info.kingcounty.gov/assessor/esales/Glossary.aspx?type=r#b 
+# Building Grade = construction quality 1-13 with 1-3 being low 
+# ZHVI = ZHVI represents the whole housing stock and not just the homes that 
+# list or sell in a given month ZHVI = zillow home value index
+# ZHVI = zillow estimated price
+# Imp Val = Anything done to raw land with the intention of increasing its value.
+# A structure erected on the property constitutes one very common type of 
+# improvement, although other actions, such as those taken to improve drainage,
+# are also improvements.
